07980f81c00d56fa14a05a20054a913fb4d8a202
Omar Alvarez <omaraleyser@hotmail.com>
2025-05-11 19:17:40 -0500

    Add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Project integrate V",
+    version="0.0.1",
+    author="Omar Alvarez",
+    author_email="",
+    description="",
+    py_modules=["activate_1","activate_2"],
+    install_requires=[
+        "pandas==2.2.3",
+        "openpyxl",
+        "requests==2.32.3",
+        "beautifulsoup4==4.13.3",
+        "scikit-learn>=0.24.0"
+    ]
+) 
